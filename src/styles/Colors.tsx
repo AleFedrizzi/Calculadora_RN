95f2584e21c37625186c9759a8b6a60d6218efb9
Typodigital <AleFedrizzi>
2022-07-11 19:37:54 -0300

    first commit

---
+++ src/styles/Colors.tsx
@@ -0,0 +1,12 @@
+// Stylos de cores da calculadora
+export const myColors = {
+  light: '#F1F2F3',
+  dark: '#17171C',
+  blue: '#4B5EFC',
+  btnGray: '#4e505f',
+  btnDark: '#2e2f38',
+  gray: '#747477',
+  black: '#000000',
+  white: '#FFFFFF',
+  result: '#46d582'
+}
